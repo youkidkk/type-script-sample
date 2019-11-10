3e5772e99e79656d1a12fc676e1b654db1fdf62d
youkidkk <youkidkk80@gmail.com>
2019-11-10 17:34:14 +0900

    basic に追加

---
+++ src/basic/literal_types.ts
@@ -0,0 +1,13 @@
+// String literal
+let fname: "Ichiro" | "Jiro" | "Saburo";
+fname = "Ichiro";
+fname = "Jiro";
+console.log(fname);
+// fname = "Shiro" -> Error
+
+// Numeric literal
+let numlet: 1 | 2 | 3;
+numlet = 1;
+numlet = 2;
+console.log(numlet);
+// numlet = 4 -> Error
---
+++ src/basic/union_nullable.ts
@@ -0,0 +1,13 @@
+// Union
+let union: number | string;
+union = 123;
+console.log(union);
+union = "text";
+console.log(union);
+
+// Nullable
+let nullable: number | null;
+nullable = null;
+console.log(nullable);
+nullable = 123;
+console.log(nullable);

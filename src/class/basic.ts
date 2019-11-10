97c30e8ef89e89aa0e27ec6c3d9b681dd34e0921
youkidkk <youkidkk80@gmail.com>
2019-11-10 20:22:34 +0900

    class/basic を追加

---
+++ src/class/basic.ts
@@ -0,0 +1,33 @@
+class ClassSample {
+  // public field
+  public publicField: string;
+  // private field
+  private privateField: string;
+  // default -> public field
+  defaultField: string;
+
+  constructor(publicField: string, privateField: string, defaultField: string) {
+    this.publicField = publicField;
+    this.privateField = privateField;
+    this.defaultField = defaultField;
+  }
+
+  public publicFunction(): void {
+    console.log(`
+      Public:  ${this.publicField}
+      Private: ${this.privateField}
+      Default: ${this.defaultField}
+    `);
+  }
+}
+
+const sample = new ClassSample(
+  "public sample",
+  "private sample",
+  "default sample"
+);
+sample.publicFunction();
+sample.publicField = "public!!!";
+sample.defaultField = "default!!!";
+// sample.privateField = "private!!!"; -> Error
+sample.publicFunction();

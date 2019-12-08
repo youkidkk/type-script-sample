8603a68bdf9eff866968fcb4c60460f49ccba93d
youkidkk <youkidkk80@gmail.com>
2019-12-08 10:05:31 +0900

    コメント変更

--- src/basic/basic_type.ts
+++ src/basic/basic_type.ts
@@ -1,45 +1,45 @@
-// Type: boolean -> true/false
+// boolean : true/false
 let bool: boolean;
 bool = false;
 bool = true;
 console.log(bool);
 
-// Type: number
+// number
 let num: number;
 num = 123;
 console.log(num);
 num = 0.123;
 console.log(num);
-// binary
+// number - binary
 num = 0b0101;
 console.log(num);
-// hex
+// number - hex
 num = 0xff;
 console.log(num);
 
-// Type: string
+// string
 let str: string;
 str = "Type";
 str += "Script";
 console.log(str);
 
-// Type: array
+// array
 const ary: number[] = [1, 2, 3];
 ary.push(4, 5);
 console.log(ary);
 
-// Type: tuple
+// tuple
 const tpl: [string, number] = ["text", 123];
 console.log(tpl);
 // tpl = [123, 234]; -> type error
 
-// Type: any
+// any
 const anyary: any[] = [];
 anyary.push(123);
 anyary.push("string");
 console.log(anyary);
 
-// Type: void
+// void
 function voidFunction(): void {
   console.log("Void function");
 }
